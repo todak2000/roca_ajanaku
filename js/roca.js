efd2ed0c8cf51b7ac4a2663c8055b3721521ec1a
Daniel Olagunju <todak2000@gmail.com>
2021-04-11 19:08:02 +0100

    buggy

--- js/roca.js
+++ js/roca.js
@@ -1,44 +1,62 @@
 // JOIN US API
+
 $(function(){
-    $('#send_submit').on('click', function (e) {
-        e.preventDefault();
-        
-        let name = document.getElementById("name").value;
-        let email = document.getElementById("email").value;
-        let phonenumber = document.getElementById("phonenumber").value;
-        let attach = document.getElementById("attach").files[0];
+    $('#send_submit').on('click', function(){ 
+        var fd = new FormData($("#fileinfo"));
+        //fd.append("CustomField", "This is some extra data");
         $.ajax({
-            // url:'http://127.0.0.1:8000/server/send/',
-            url:'https://roca-ajanaku.herokuapp.com/server/send',
-            type:'POST',
-            data:{
-                name: name,
-                email: email,
-                phonenumber: phonenumber,
-                attach : attach 
+            url: 'https://roca-ajanaku.herokuapp.com/server/send',  
+            type: 'POST',
+            data: fd,
+            success:function(data){
+                console.log(data)
             },
-            success:function(response){
-                console.log(response);
-                // if(response.error == false){
-                //     document.getElementById('modal_name').innerHTML = response.name;
-                //     document.getElementById('modal_date').innerHTML = response.date;
-                //     document.getElementById('modal_time').innerHTML = response.from +" - "+response.to;
-                //     $('#successModal').modal('show');
-                    
-                // }
-                // else{
-                //     document.getElementById('server_message_success').innerHTML = response.message;
-                //     document.getElementById("server_message_success").style.display = "block";
-                //     setTimeout(function(){ 
-                //         document.getElementById("server_message_success").style.display = "none"; 
-                //     }, 3000);
-                // }
-            },
-            error:function(e){
-                console.log(e);
-            },
+            cache: false,
+            contentType: false,
+            processData: false
         });
-        
-        
     });
 });
+
+// $(function(){
+//     $('#send_submit').on('click', function (e) {
+//         e.preventDefault();
+        
+//         let name = document.getElementById("name").value;
+//         let email = document.getElementById("email").value;
+//         let phonenumber = document.getElementById("phonenumber").value;
+//         let attach = document.getElementById("attach").files[0];
+//         $.ajax({
+//             url:'https://roca-ajanaku.herokuapp.com/server/send',
+//             type:'POST',
+//             data:{
+//                 name: name,
+//                 email: email,
+//                 phonenumber: phonenumber,
+//                 attach : attach 
+//             },
+//             success:function(response){
+//                 console.log(response);
+//                 // if(response.error == false){
+//                 //     document.getElementById('modal_name').innerHTML = response.name;
+//                 //     document.getElementById('modal_date').innerHTML = response.date;
+//                 //     document.getElementById('modal_time').innerHTML = response.from +" - "+response.to;
+//                 //     $('#successModal').modal('show');
+                    
+//                 // }
+//                 // else{
+//                 //     document.getElementById('server_message_success').innerHTML = response.message;
+//                 //     document.getElementById("server_message_success").style.display = "block";
+//                 //     setTimeout(function(){ 
+//                 //         document.getElementById("server_message_success").style.display = "none"; 
+//                 //     }, 3000);
+//                 // }
+//             },
+//             error:function(e){
+//                 console.log(e);
+//             },
+//         });
+        
+        
+//     });
+// });
