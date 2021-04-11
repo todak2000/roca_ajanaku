aeb5b861f755f9601c7c097e854d445c7b05d3dc
Daniel Olagunju <todak2000@gmail.com>
2021-04-11 05:19:11 +0100

    com

--- js/roca.js
+++ js/roca.js
@@ -9,7 +9,7 @@ $(function(){
         let attach = document.getElementById("attach").files[0];
         $.ajax({
             // url:'http://127.0.0.1:8000/server/send/',
-            url:'https://roca-ajanaku.herokuapp.com/server/send/',
+            url:'https://roca-ajanaku.herokuapp.com/server/send',
             type:'POST',
             data:{
                 name: name,
